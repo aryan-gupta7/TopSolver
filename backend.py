87a8991703a3da23cae23e39bb899f21b38a01a1
akshat srivastava <64839659+Jrkej@users.noreply.github.com>
2020-10-01 13:32:39 +0530

    Small changes

--- backend.py
+++ backend.py
@@ -1,13 +1,13 @@
 from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/")
 def main():
     return redirect(url_for("home"))
 
-@app.route("/home")
+@app.route("/home",methods=["GET","POST"])
 def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
